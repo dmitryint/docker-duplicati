114ae71f90f4d4139bde3617c46d83db2b61093e
Dmitry K <d.p.karpov@gmail.com>
2016-02-04 23:14:26 +0300

    Attempt to repair local database, if the command exits with an error.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,9 @@ if [ -z "$1" ]; then
 	exec $DUPLICATI_CMD --webservice-port=8200 --webservice-interface=* --webservice-password=${DUPLICATI_PASS}
 else
 	$DUPLICATI_CMD $@
+	if [ "$?" -eq 100 ] && [ -n "$ENABLE_AUTO_REPAIR" ]; then
+		echo "Trying to repair local storage."
+		$DUPLICATI_CMD $(echo $@ | sed "s/^\w*\s/repair /g" | sed -r 's/[* ]\/[a-zA-Z_].*+//')
+		$DUPLICATI_CMD $@
+	fi
 fi
