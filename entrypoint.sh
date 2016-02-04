792024362601ce9c8f82fed1ca7d21761ead7879
Dmitry K <d.p.karpov@gmail.com>
2016-02-04 23:11:04 +0300

    replace command by environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+DUPLICATI_CMD='mono /app/Duplicati.CommandLine.exe'
+
 if [ -z "$1" ]; then
     if [ ! -n "$DUPLICATI_PASS" ]; then
       echo "ERROR- DUPLICATI_PASS must be defined"
       exit 1
     fi
-	exec mono /app/Duplicati.Server.exe --webservice-port=8200 --webservice-interface=* --webservice-password=${DUPLICATI_PASS}
+	exec $DUPLICATI_CMD --webservice-port=8200 --webservice-interface=* --webservice-password=${DUPLICATI_PASS}
 else
-	mono /app/Duplicati.CommandLine.exe $@
+	$DUPLICATI_CMD $@
 fi
